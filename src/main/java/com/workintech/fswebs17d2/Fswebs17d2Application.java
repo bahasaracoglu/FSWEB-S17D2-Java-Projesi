dc30bd62d389b035444833d64b49f7a1aa0c11f5
bahasaracoglu <bahasaracoglu@gmail.com>
2023-08-22 22:55:27 +0300

    First init

---
+++ src/main/java/com/workintech/fswebs17d2/Fswebs17d2Application.java
@@ -0,0 +1,13 @@
+package com.workintech.fswebs17d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fswebs17d2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fswebs17d2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/fswebs17d2/Fswebs17d2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.fswebs17d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fswebs17d2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
