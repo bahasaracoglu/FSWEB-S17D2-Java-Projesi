581d536281382aeb6781c53aa8fb983d79f5ae22
bahasaracoglu <bahasaracoglu@gmail.com>
2023-08-22 23:13:25 +0300

    developer and subclasses created.

---
+++ src/main/java/model/Developer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Developer {
+    private int id;
+    private String name;
+    private int salary;
+    private Experience experience;
+
+    public Developer(int id, String name, int salary, Experience experience) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.experience = experience;
+    }
+}
---
+++ src/main/java/rest/DeveloperController.java
@@ -0,0 +1,7 @@
+package rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeveloperController {
+}
